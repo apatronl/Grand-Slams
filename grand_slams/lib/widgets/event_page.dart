f5ed47b2741c44094da261cdf7a45c6595db67ff
apatronl <ale7feb@hotmail.com>
2019-05-30 22:07:15 -0400

    A working mess 😅

--- grand_slams/lib/widgets/event_page.dart
+++ grand_slams/lib/widgets/event_page.dart
@@ -4,33 +4,101 @@ import 'package:webview_flutter/webview_flutter.dart';
 class EventPage extends StatelessWidget {
   final Map<String, dynamic> eventData;
   const EventPage({Key key, this.eventData}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
+    final eventDataList = [eventData['summary'], 'Visit website'];
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
           color: Colors.black87,
+          
         ),
-        title: Text(
-          eventData['name'],
-          style: TextStyle(
-            color: Colors.black87,
-            fontWeight: FontWeight.bold,
-          ),
+        title: new Image.asset(
+          'assets/${eventData['logo']}', 
+          fit: BoxFit.cover,
         ),
         backgroundColor: Colors.white,
         brightness: Brightness.light,
         elevation: 0.0,
       ),
-      body: Center(
-        child: WebView(
-          initialUrl: eventData['website'],
-          javascriptMode: JavascriptMode.unrestricted,
+      body: Container(
+        child: ListView.separated(
+          separatorBuilder: (context, index) => Divider(
+            color: Colors.grey,
+          ),
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: eventDataList.length,
+          itemBuilder: (BuildContext context, int index) {
+            switch (index) {
+
+              case 0: {
+                return Container(
+                  child: Text(
+                    eventDataList[index],
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  margin: new EdgeInsets.symmetric(
+                    horizontal: 16.0, 
+                    vertical: 16.0,
+                  ),
+                );
+              }
+              break;
+
+              case 1: {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute<void>(
+                      builder: (BuildContext context) {
+                        return Scaffold(
+                          appBar: AppBar(
+                            iconTheme: IconThemeData(
+                              color: Colors.black87,
+                            ),
+                            title: Text(
+                              eventData['name'],
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            backgroundColor: Colors.white,
+                            brightness: Brightness.light,
+                            elevation: 0.0,
+                          ),
+                          body: Center(
+                            child: WebView(
+                              initialUrl: eventData['website'],
+                              javascriptMode: JavascriptMode.unrestricted,
+                            ),
+                          ),
+                        );
+                      },
+                    ));
+                  },
+                  child: Container(
+                    child: Text(
+                      eventDataList[index],
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    margin: new EdgeInsets.symmetric(
+                      horizontal: 16.0, 
+                      vertical: 16.0,
+                    ),
+                  ),
+                );
+              }
+              break;
+            }
+          },
         ),
-        // child: Text(
-        //   eventData['summary'],
-        // ),
       ),
     );
   }
